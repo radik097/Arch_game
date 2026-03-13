0fdf9ae620039fb43056a316369366e242330055
radik098 <rodionmoroz98@gmail.com>
2026-03-13 21:38:47 +1100

    Signed-off-by: radik098 <rodionmoroz98@gmail.com>
    	modified:   .github/workflows/jekyll-gh-pages.yml
    	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [react()],
   test: {
     environment: 'node',
