eda7628c4327b1aa7dab23df5ead5bb6ceb9e65c
radik097 <radikmoroz98@gmail.com>
2026-03-19 07:44:04 -0700

    fix: set BrowserRouter basename and add diagnostic build logging

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,12 @@ import { App } from './app/App';
 import { StatsPage } from './app/StatsPage';
 import './styles/app.css';
 
-const isStatsRoute = window.location.pathname === '/stats';
+const base = import.meta.env.BASE_URL;
+const isStatsRoute = window.location.pathname.replace(/\/+$/, '') === (base + 'stats').replace(/\/+$/, '').replace(/\/+/g, '/');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={base}>
       {isStatsRoute ? <StatsPage /> : <App />}
     </BrowserRouter>
   </React.StrictMode>,
